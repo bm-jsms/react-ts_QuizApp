3ff4057263d31f71c1a38b83caaf6ce671c52e8a
Josué EmaxS <josueemaxsalazar04@gmail.com>
2024-11-21 04:46:28 -0500

    🗃️ [create] questions - data provider

---
+++ src/data/questions.ts
@@ -0,0 +1,9 @@
+import type { QuestionType } from '../types';
+
+export const questions: QuestionType[] = [
+	{
+		id: 'q1',
+		text: 'Question?',
+		answers: ['Answer 1', 'Answer 2', 'Answer 3', 'Answer 4'],
+	},
+];
