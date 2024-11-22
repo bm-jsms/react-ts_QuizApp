0be484fade8d33df25664dd56d0959dca4ae58e4
Josué EmaxS <josueemaxsalazar04@gmail.com>
2024-11-22 03:43:30 -0500

    🗃️ [add] more questions template

--- src/data/questions.ts
+++ src/data/questions.ts
@@ -6,4 +6,19 @@ export const questions: QuestionType[] = [
 		text: 'Question?',
 		answers: ['Answer 1', 'Answer 2', 'Answer 3', 'Answer 4'],
 	},
+	{
+		id: 'q2',
+		text: 'Question2?',
+		answers: ['Answer 1', 'Answer 2', 'Answer 3', 'Answer 4'],
+	},
+	{
+		id: 'q3',
+		text: 'Question3?',
+		answers: ['Answer 1', 'Answer 2', 'Answer 3', 'Answer 4'],
+	},
+	{
+		id: 'q4',
+		text: 'Question4?',
+		answers: ['Answer 1', 'Answer 2', 'Answer 3', 'Answer 4'],
+	},
 ];
