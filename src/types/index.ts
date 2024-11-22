7b97b07dcf9ae11bd21cd5c47ecd3bcd3c9b15cf
Josué EmaxS <josueemaxsalazar04@gmail.com>
2024-11-22 06:02:59 -0500

    🚀 [export] types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,5 @@
 export type { QuestionType } from './Question.type';
+export type { SummaryProps } from './SummaryProps.type';
+export type { QuestionTimerProps } from './QuestionTimerProps.type';
+export type { QuestionProps } from './QuestionProps.type';
+export type { AnswersProps } from './AnswersProps.type';
